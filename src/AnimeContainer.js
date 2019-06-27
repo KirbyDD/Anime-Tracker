d93a426599dfe06c5dd093002b1f8d4714a2e359
DeMarcus Kirby <tkd024@gmail.com>
2019-06-27 09:03:47 -0600

    Added Navbar and AnimeContainer components

---
+++ src/AnimeContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class AnimeContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      current: false
+    }
+  }
+
+  render() {
+    return (
+      null
+    )
+  }
+}
+
+export default AnimeContainer;
---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedTab: 'Home'
+    }
+  }
+
+  render() {
+    return (
+      null
+    )
+  }
+}
+
+export default Navbar;
