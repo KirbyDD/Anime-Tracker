5b7cdbf265df03b9f38c0a934eef793461c0c806
DeMarcus Kirby <tkd024@gmail.com>
2019-07-25 13:24:05 -0600

    added hover state to show title of anime

--- src/Components/AnimeSnippet/AnimeSnippet.js
+++ src/Components/AnimeSnippet/AnimeSnippet.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom'
 import './AnimeSnippet.scss'
 
-const AnimeSnippet = props => {
-  return (
-    <Link to={`/${props.enTitle}`}>
-      <img src={props.posterImage.small} alt={`poster for ${props.enTitle}`} className="snippet-image"/>
-    </Link>
-  )
+class AnimeSnippet extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      tite: false
+    }
+  }
+
+  handleEnter = () => {
+    this.setState({ title: true })
+  }
+
+  handleLeave = () => {
+    this.setState({ title: false })
+  }
+
+  titleToDisplay = () => {
+    if(this.props.enTitle){
+      return this.props.enTitle
+    } else {
+      return this.props.jpTitle
+    }
+  }
+
+  render() {
+    let title = this.titleToDisplay()
+    return (
+      <Link to={`/${this.props.enTitle}`}>
+      {this.state.title && <p className='snippet-title'>{title}</p>}
+        <img src={this.props.posterImage.small} 
+              alt={`poster for ${this.props.enTitle}`} 
+              className="snippet-image"
+              onMouseEnter={e => this.handleEnter()}
+              onMouseLeave={e => this.handleLeave()}
+              />
+      </Link>
+    )
+  }
 }
 
 export default AnimeSnippet;
