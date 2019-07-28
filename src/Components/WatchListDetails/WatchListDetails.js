b5cbaabcdd5e35e0714e3e3d1209b0c9669856fd
DeMarcus Kirby <tkd024@gmail.com>
2019-07-28 17:42:41 -0600

    Add episode selector in Watchlist details

--- src/Components/WatchListDetails/WatchListDetails.js
+++ src/Components/WatchListDetails/WatchListDetails.js
@@ -1,15 +1,38 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import { optionalCallExpression } from '@babel/types';
 
 class WatchListDetails extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentEpisode: 1
+    }
+  }
+
+  numOptions = () => {
+    let options = []
+    for(let i = 1; i <= this.props.episodes; i++){
+        options.push(<option value={i}>{i}</option>)
+      if(options.length === this.props.episodes){
+        return options
+      }
+    }
+  }
+
+  updateEpisode = e => {
+    this.setState({ currentEpisode: e.target.value })
   }
 
   render() {
     return(
       <section>
         <h2>{this.props.enTitle}</h2>
+        <h3>{this.props.synopsis}</h3>
+        <select onChange={e => this.updateEpisode(e)}>
+          {this.numOptions()}
+        </select>
+
       </section>
     )
   }
