0ab6c2ab1836b3402e573e56a692ebf40f7b3bde
DeMarcus Kirby <tkd024@gmail.com>
2019-07-29 11:08:49 -0600

    Add Styling to watchlist container

--- src/Components/WatchListContainer/WatchListContainer.js
+++ src/Components/WatchListContainer/WatchListContainer.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import {connect} from 'react-redux';
 import AnimeSnippet from '../AnimeSnippet/AnimeSnippet';
+import './WatchListContainer.scss'
 
 const WatchListContainer = props => {
   let list = props.followedShows.map(anime => {
       return <AnimeSnippet {...anime} isFav={true} />   
   })
   return (
-    <section>
+    <section className='watchlist'>
       {list}
     </section>
   )
