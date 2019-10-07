b28c687f6673c0dcf50278c8291541aca614e4e7
DeMarcus Kirby <tkd024@gmail.com>
2019-10-07 10:29:50 -0600

    Add more terms to dictionary

--- src/Components/Dictionary/Dictionary.js
+++ src/Components/Dictionary/Dictionary.js
@@ -6,15 +6,29 @@ const Dictionary = () => {
     <section className='dictionary'>
       <h2 className='title'>Anime Community Dictionary</h2>
       <ul>
-        <li>Anime (ah-knee-meh): Used mostly to refer to animation specifically from Japan</li>
-        <li>Tsundere (soon-deh-reh): A character who treats their love interest harshly because they are too afraid to express their true feelings.</li>
-        <li>Weeb (we-b): A Person who either tries to you anime specific lingo in everyday life or 
+        <li><strong>Anime (ah-knee-meh): </strong>Used mostly to refer to animation specifically from Japan</li>
+        <li><strong>Dandere (dahn-deh-reh): </strong>Character that show little to zero emotion externally even though they can internally feel extreme emotions.</li>
+        <li><strong>Dub: </strong>Anime that after being recorded in it's original language has the voices rerecorded in a new language.</li>
+        <li><strong>Ecchi (eh-chee): </strong>Anime Category that centers around sexual situations but not actual sex.</li>
+        <li><strong>Filler: </strong>An episode that does not progress the story. Some examples are flashback episodes or beach vacation episodes.</li>
+        <li><strong>Isekai (E-seh-kai): </strong>Anime Category where the main character is transported to a new world/dimension.</li>
+        <li><strong>Kouhai: </strong>A person who is an under classman or has less experience in a work place.</li>
+        <li><strong>Mecha (meh-kah): </strong>Anime Category that focuses on characters piloting a large mechanized combat robot.</li>
+        <li><strong>Onii-san/chan (Oh-knee): </strong>Older Brother.</li>
+        <li><strong>Onee-san/chan (Oh-neh): </strong>Older Sister.</li>
+        <li><strong>O.V.A. (Original Video Animation): </strong>A one off episode/movie that is seperate from the current stories progression.</li>
+        <li><strong>Senpai (sin-pie): </strong>A person who is an upper classman or has more experience in a work place.</li>
+        <li><strong>Slice of Life: </strong>Anime Category that depicts character in a natural day to day life.</li>
+        <li><strong>Sub: </strong>Anime that after being recorded in original language is then given subtitles to a different language.</li>
+        <li><strong>Tsundere (soon-deh-reh): </strong>A character who treats their love interest harshly because they are too afraid to express their true feelings.</li>
+        <li><strong>Weeb (we-b): </strong>A Person who either tries to you anime specific lingo in everyday life or 
           tries to incorporate Japanese customs into their personal life base mostly off watching anime.
         </li>
-        <li>Yandere (yahn-deh-reh): A character that is so in love with someone else that they are willing to hurt/kill someone over their love interest for minor
+        <li><strong>Yandere (yahn-deh-reh): </strong>A character that is so in love with someone else that they are willing to hurt/kill someone over their love interest for minor
           situations like greetings, conversing, or even looking in their direction. Also willing to harm love interest
           if feeling betrayed. 
         </li>
+        <li><strong></strong></li>
       </ul>
     </section>
   )
